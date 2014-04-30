31a5bafd34eda58103aa8cae5ef762e4ac1ac19a
hover <david.anzenhofer@hs-augsburg.de>
2014-04-30 11:25:01 +0200

    test

--- hello.c
+++ hello.c
@@ -19,7 +19,7 @@ int main( int argc, char ** argv )
    for( i = 0; i < 10; i++ )
 #endif
 <<<<<<< HEAD
-      puts( "Michi ist toll ! :o...... Absolut" );
+      puts( "Michi ist toll ! :o...... Absolut!!" );
 =======
       printf( "Michi ist toll ! :o" );
 >>>>>>> 2413dc861c71301e573e65aad461c1a0341c7206
