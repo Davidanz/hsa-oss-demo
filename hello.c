5396a0c68e74dc859427319ca50a4671f8b2397f
Weiss Christian <christian.weiss@hs-augsburg.de>
2014-04-30 10:20:09 +0200

    Code verändert

--- hello.c
+++ hello.c
@@ -8,9 +8,15 @@ int main( int argc, char ** argv )
 {
 #ifdef CONFIG_REPEAT
    int i;
-   for( i = 0; i < 10; i++ )
+   for( i = 0; i < 10; i++ ) {
 #endif
-      puts( "Hello World xxx!" );
+      puts( "Hello World xxx!  " );
+
+#ifdef CONFIG_REPEAT
+      puts( i );
+      puts( "\n");
+   }
+#endif
 
    return 0;
 }
